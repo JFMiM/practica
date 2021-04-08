867115218ea1b2641d96ca1a1c79a0d19daef0d5
Juan Fernando Miranda Monroy <fer18@gmail.com>
2021-04-07 20:21:49 -0500

    Segunda Version

--- unidad3/views.py
+++ unidad3/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Template,Context
 
 # Create your views here.
+def practica(request):
+    externo = open("C:/Users/USER/Desktop/python/practica/fer/index.html")
+    plantilla = Template(externo.read())
+    externo.close()
+    ctx = Context()
+    contenido = plantilla.render(ctx)
+    return HttpResponse(contenido)
